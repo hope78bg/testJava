32f9ecb4db8a1b115c80c21855d73b987da10dc8
Nadia Georgieva <hope78bg@yahoo.com>
2016-03-21 11:10:08 +0200

    Reading the array from console.

--- test_01/src/ArrayReverseExample.java
+++ test_01/src/ArrayReverseExample.java
@@ -1,9 +1,38 @@
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class ArrayReverseExample {
     public static void main(String[] args) {
-        int[] array = new int[] {1, 2, 3, 4, 5};
-        int length = array.length;
+        System.out.print("Enter some numbers separated by commas: ");
+
+        Scanner input = new Scanner(System.in);
+        String line = input.nextLine();
+        input.close();
+
+        List<Integer> l = new LinkedList<Integer>();
+
+        Scanner ns = new Scanner(line);
+        Pattern pat = Pattern.compile("\\s*,\\s*");
+        ns.useDelimiter(pat);
+
+        try {
+            while (ns.hasNextInt()) {
+                l.add(ns.nextInt());
+            }
+        }
+        finally {
+            ns.close();
+        }
+
+        int length = l.size();
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = l.remove(0);
+        }
+
         int[] reversed = new int[length];
         for (int index = 0; index < length; index++) {
             reversed[length- index -1] = array[index];
